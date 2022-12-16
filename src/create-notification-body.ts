d7b3fe7f9524f6f6f086a62f62ec9e0559ddebd7
João Bispo <joaobispo2077@gmail.com>
2022-12-16 17:18:20 -0300

    feat: add validations

---
+++ src/create-notification-body.ts
@@ -0,0 +1,13 @@
+import { IsNotEmpty, IsUUID, Length } from 'class-validator';
+
+export class CreateNotificationBody {
+  @IsNotEmpty()
+  @Length(5, 240)
+  content: string;
+
+  @IsNotEmpty()
+  category: string;
+
+  @IsUUID()
+  recipientId: string;
+}

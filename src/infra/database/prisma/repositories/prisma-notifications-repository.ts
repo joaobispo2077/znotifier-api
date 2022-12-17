d43b3ead00fb4725e1c2974953678e3b39884d16
João Bispo <joaobispo2077@gmail.com>
2022-12-17 14:15:08 -0300

    feat: add prisma notification repository

---
+++ src/infra/database/prisma/repositories/prisma-notifications-repository.ts
@@ -0,0 +1,20 @@
+import { Notification } from 'src/application/entities/notification';
+import { NotificationsRepository } from 'src/application/repositories/notifications-repository';
+import { PrismaService } from '../prisma.service';
+
+export class PrismaNotificationsRepository implements NotificationsRepository {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(notification: Notification): Promise<void> {
+    await this.prismaService.notification.create({
+      data: {
+        id: notification.id,
+        category: notification.category,
+        content: notification.content.value,
+        recipientId: notification.recipientId,
+        createdAt: notification.createdAt,
+        readAt: notification.readAt,
+      },
+    });
+  }
+}

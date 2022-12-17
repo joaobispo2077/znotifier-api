741c9503dd9d892b353d1a3ec383dcac4247597e
João Bispo <joaobispo2077@gmail.com>
2022-12-17 13:25:42 -0300

    feat: add notificaiton class

---
+++ src/application/entities/notification.ts
@@ -0,0 +1,61 @@
+import { Replace } from 'src/helpers/Replace';
+import { Content } from './content';
+
+export interface NotificationsData {
+  recipientId: string;
+  content: Content;
+  category: string;
+  readAt?: Date | null;
+  createdAt: Date;
+}
+
+export class Notification {
+  private data: NotificationsData = {} as NotificationsData;
+
+  constructor(
+    data: Replace<
+      NotificationsData,
+      {
+        createdAt?: Date;
+      }
+    >,
+  ) {
+    this.data = { ...data, createdAt: data.createdAt || new Date() };
+  }
+
+  public set recipientId(recipientId: NotificationsData['recipientId']) {
+    this.data.recipientId = recipientId;
+  }
+
+  public get recipientId(): NotificationsData['recipientId'] {
+    return this.data.recipientId;
+  }
+
+  public set content(content: NotificationsData['content']) {
+    this.data.content = content;
+  }
+
+  public get content(): NotificationsData['content'] {
+    return this.data.content;
+  }
+
+  public set category(category: NotificationsData['category']) {
+    this.data.category = category;
+  }
+
+  public get category(): NotificationsData['category'] {
+    return this.data.category;
+  }
+
+  public set readAt(readAt: NotificationsData['readAt']) {
+    this.data.readAt = readAt;
+  }
+
+  public get readAt(): NotificationsData['readAt'] {
+    return this.data.readAt;
+  }
+
+  public get createdAt(): NotificationsData['createdAt'] {
+    return this.data.createdAt;
+  }
+}
